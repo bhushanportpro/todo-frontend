ced24814358e372ace28c0eb3426efe4dedd56d5
bhushanportpro <bhushan@portpro.io>
2026-09-16 10:23:06 +0545

    Configure for static output

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 'standalone' produces a minimal self-contained build - ideal for Docker later
-  output: "standalone",
+  output: "export",
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://backend:4000/api/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
